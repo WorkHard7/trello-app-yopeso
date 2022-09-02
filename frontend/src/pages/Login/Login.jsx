e84879a3331b93f6de4567d2eb40b8f4a130f044
Bogdan Triboi <btriboi@tests-MacBook-Pro.local>
2022-09-02 14:27:37 +0300

    Created login page + added few style variables

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import "./Login.scss"
+import {LoginCard} from "../../components/Login/LoginCard"
+
+export const Login = () => {
+    return (
+
+    <div class="login">
+        <h1>Trello</h1>
+        <LoginCard/>
+    </div>
+    
+    )
+
+}
