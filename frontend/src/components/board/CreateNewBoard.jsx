4da691ac9c8626aa0e31455e55aee3b45680e2d6
Mihu <solinuxnet@gmail.com>
2022-09-06 13:07:00 +0300

    - Deleted DataFetching folder and file - Small changes in BoardButton.scss - Added connection with backend when creating a new board

--- frontend/src/components/board/CreateNewBoard.jsx
+++ frontend/src/components/board/CreateNewBoard.jsx
@@ -1,26 +1,33 @@
 import './BoardButton.scss';
 import React, {useState, useEffect, useRef} from "react";
 import {useNavigate} from "react-router-dom";
+import axios from 'axios';
 
-const colors = ["#0079bf", "#d29034", "#519839", "#b04732", "#89609e"];
-
-function Popup(props) {
+function PopUpForm(props) {
     const [currentColor, setColor] = useState(colors[0]);
-    const [inputMessage, setInputMessage] = useState('');
+    const [inputTitle, setInputTitle] = useState('');
     const createBtn = useRef(null);
 
+
     const handleClick = (e) => {
-        if (inputMessage.trim().length === 0) return
-        props.navigate(`/board/id`)
+        if (inputTitle.trim().length === 0) return
+        createBoard(inputTitle, colorToName.get(currentColor))
+            .then((res) => res.data.id)
+            .then((id) => props.navigate(`/board/` + id))
+            .catch(err => {
+                if(err.response.status === 401){
+                    props.navigate(`/signin`);
+                }
+            })
     };
 
     useEffect(() => {
-        if (inputMessage.trim().length === 0) {
+        if (inputTitle.trim().length === 0) {
             createBtn.current.style.backgroundColor = 'lightblue';
         } else {
             createBtn.current.style.backgroundColor = 'blue';
         }
-    }, [inputMessage]);
+    }, [inputTitle]);
 
     return (
         <div className="create-board-container">
@@ -39,22 +46,49 @@ function Popup(props) {
                         />
                     ))}
                 </div>
-                <form className="title-input">
+                <div className="title-input">
                     <label className="board-title" htmlFor="board-title">Board title</label>
-                    <input onChange={(e) => setInputMessage(e.target.value)} value={inputMessage} type='text'
+                    <input onChange={(e) => setInputTitle(e.target.value)} value={inputTitle} type='text'
                            className="board-title-input" placeholder='required field' autoFocus required/>
                     <button ref={createBtn} onClick={() => {
                         handleClick()
                     }} className="createBtn">Create
                     </button>
-                </form>
+                </div>
             </div>
         </div>
     );
 }
 
+const colors = ["#0079bf", "#d29034", "#519839", "#b04732", "#89609e"];
 
-const CreateBoardButton = () => {
+const colorToName = new Map([
+    ['#0079bf', 'blue'],
+    ['#d29034', 'orange'],
+    ['#519839', 'green'],
+    ['#b04732', 'red'],
+    ['#89609e', 'violet']
+])
+
+const createBoard = (title, color) => {
+
+    const jwtToken = localStorage.getItem('JWT');
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwtToken
+    }
+
+    const requestBody = {title, color};
+    const url = 'http://localhost:8089/api/boards';
+
+    return axios.post(`${url}`, requestBody, {
+        headers: headers
+    })
+}
+
+
+const CreateNewBoard = () => {
     const [popupIsVisible, SetTogglePopup] = useState(null);
     const navigate = useNavigate();
 
@@ -63,11 +97,11 @@ const CreateBoardButton = () => {
             <div className="create-board-top" onClick={() => SetTogglePopup(!popupIsVisible)}>
                 <button className="create-board-btn">Create new board</button>
             </div>
-            {popupIsVisible && <div className='fadeIn'><Popup navigate={navigate}/></div>}
-            {popupIsVisible === false && <div className='fadeOut'><Popup/></div>}
+            {popupIsVisible && <div className='fadeIn'><PopUpForm navigate={navigate}/></div>}
+            {popupIsVisible === false && <div className='fadeOut'><PopUpForm/></div>}
         </div>
     );
 }
 
-export default CreateBoardButton;
+export default CreateNewBoard;
 
