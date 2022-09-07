6d5c51749901706b0ffec902c0268016cabdced2
Octavian <40757361+ooctavian@users.noreply.github.com>
2022-09-08 00:57:48 +0300

    Added requirements and docs

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,17 +4,13 @@ import { LoginCard } from "./LoginCard"
 
 export const Login = () => {
     return (
-
     <div className="login">
-
         <div className="login-header">
             <Logo/>
             <h1>Tasks</h1>
         </div>
-
         <LoginCard/>
     </div>
-    
     )
 
 }
