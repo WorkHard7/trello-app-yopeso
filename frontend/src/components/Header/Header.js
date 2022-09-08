6a644f43b74d8f567ec9604a4bcf915fde43e460
Octavian <40757361+ooctavian@users.noreply.github.com>
2022-09-08 17:31:01 +0300

    Update user info

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,31 +3,27 @@ import {BsFillGrid3X3GapFill} from "react-icons/bs";
 import {BsBell} from "react-icons/bs";
 import {AiOutlineInfoCircle} from "react-icons/ai";
 import Header1 from './Header.scss';
+import { Link } from "react-router-dom";
+import extractPayload from "../../utils/extractPayload";
 
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
 
 export function Header() {
 
     const [data,setData] = useState([]);
     useEffect(() =>
-            setData(parseJwt(localStorage.getItem('JWT')))
+            setData(extractPayload(localStorage.getItem('JWT')))
     ,[])
 
     return (
             <div className="header">
-                <div className="logo">TaskManager
-                </div>
+                <Link to={"/"} style={{ textDecoration: 'none' }}>
+                <div className="logo">TaskManager</div>
+                </Link>
                 <div className="right-section">
+                    <Link to={"/settings/general"}>
                     <img
                         src={`https://ui-avatars.com/api/?background=random&name=${data['First name']}+${data['Last name']}&rounded=true`}/>
+                    </Link>
                 </div>
             </div>
 
