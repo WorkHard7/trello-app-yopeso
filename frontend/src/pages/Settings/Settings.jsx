7dd949e287f09dc029db4ffcd1c11064da744eea
Mihu <solinuxnet@gmail.com>
2022-09-09 00:46:13 +0300

    - Connected front with backend. Now first and last name can be changed successfully!

--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -1,24 +1,35 @@
 import {Header} from "../../components/Header/Header";
 import {Link, useNavigate} from "react-router-dom";
-import {FormProvider, useForm} from "react-hook-form";
+import {FormProvider, set, useForm} from "react-hook-form";
 import {Input} from "../../components/Input/Input";
 import axios from 'axios';
 import './Settings.scss';
 import {UserInfo} from "../../components/Settings/UserInfo/UserInfo";
 import {Navigation} from "../../components/Settings/Navigation/Navigation";
+import {useState} from "react";
 
 const Settings = () => {
 
     const methods = useForm();
-
-    const navigate = useNavigate();
+    const [message, setMessage] = useState(null);
 
     const handleValidSubmit = (values) => {
+
+        const jwtToken = localStorage.getItem('JWT');
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwtToken
+        }
+
         axios.patch('http://localhost:8089/api/settings/general',
             {
-                first_name: `${values.first_name}`,
-                last_name: `${values.last_name}`
-            }).catch(error => console.log(error))
+                firstName: `${values.first_name}`,
+                lastName: `${values.last_name}`
+            }, {
+                headers: headers
+            }).then((res) => setMessage('Name was changed!'))
+            .catch((err) => setMessage('Error during saving name'))
     }
 
     const active = 'active-link';
@@ -40,6 +51,8 @@ const Settings = () => {
                             <Input type="text" name="last_name" placeholder='Enter last name' validators={{
                                 required: "Last name is required",
                             }}/>
+
+                            {message !== null ? <p style={{color: "red"}}>{message}</p> : ''}
                             <button className='btn'>Save</button>
                         </form>
                     </FormProvider>
