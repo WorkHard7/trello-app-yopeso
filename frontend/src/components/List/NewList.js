bbeb0684ad020b2af2f8c41c6236a30d7fa05857
“benichira” <beniamin.chira@yahoo.com  git config --global user.name Beniamin Chiragit config --global user.email beniamin.chira@yopeso.com>
2022-09-08 16:14:48 +0300

    Functionality on board page

--- frontend/src/components/List/NewList.js
+++ frontend/src/components/List/NewList.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './NewList.scss'
+import './NewList.scss';
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 function NewList({displayNewList, setDisplayNewList, addList, inputHandler, inputValue}) {
 
@@ -13,17 +16,23 @@ function NewList({displayNewList, setDisplayNewList, addList, inputHandler, inpu
     if (displayNewList === false) {
         return (<>
             <div className={'new-list-container'} onClick={showAddList}>
-                <h3> + Add another list</h3>
+                <span className={'create-list-btn'}> + Add another list</span>
             </div>
         </>);
     } else {
-
-        return (<div className={'new-list-container'}>
-            <input id={'titleInput'} value={inputValue} onChange={inputHandler} type={'text'}
-                   placeholder={'Enter list title...'}/>
-            <button onClick={closeAddList}>Close</button>
-            <button onClick={addList}>Add</button>
-        </div>)
+        return (
+            <div className={'new-list-container'}>
+                <input className={'list-name-field'} id={'titleInput'} value={inputValue} onChange={inputHandler}
+                       type={'text'}
+                       placeholder={'Enter list title...'}/>
+                <div className={'create-list-btns'}>
+                    <button className={'add-list-btn'} onClick={addList}>Add list</button>
+                    <button className={'close-add-list-btn'} onClick={closeAddList}>
+                        <FontAwesomeIcon icon={faXmark}/>
+                    </button>
+                </div>
+            </div>
+        )
     }
 }
 
