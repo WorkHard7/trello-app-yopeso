6fc18414b959082eb9e2776577464fa108bd8ecb
Ana Rotariu <anarotariu@192-168-0-101.rdsnet.ro>
2022-09-09 11:50:10 +0300

    spinner for list

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -4,11 +4,12 @@ import AddCardButton from "../AddCardButton/AddCardButton";
 import axios from "axios";
 import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-
+import Loading from "../Loading/Loading";
 
 function List({board_id, token, list, cardInput, inputHandler, getAllLists}) {
 
     const [itemCardItems, setItemCardItems] = useState(list.tasks)
+    const [loading, setLoading] = useState(false);
 
     const getTask = () => {
         axios.get(`http://localhost:8089/api/boards/${board_id}/lists/${list.id}`, {
@@ -27,6 +28,8 @@ function List({board_id, token, list, cardInput, inputHandler, getAllLists}) {
     }
 
     const addCard = () => {
+        setLoading(true);
+        console.log("loading in addCard before post = ", loading);
         axios.post(`http://localhost:8089/api/boards/${board_id}/lists/${list.id}/items`, {
                 title: cardInput
             }, {
@@ -42,9 +45,11 @@ function List({board_id, token, list, cardInput, inputHandler, getAllLists}) {
             .catch((err) => {
                 console.error(err)
             })
+        setLoading(false);
     }
 
     const deleteList = () => {
+        setLoading(true);
         axios.delete(`http://localhost:8089/api/boards/${board_id}/lists/${list.id}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
@@ -57,23 +62,30 @@ function List({board_id, token, list, cardInput, inputHandler, getAllLists}) {
                 console.error(err)
             })
             .finally(() => getAllLists(board_id, token))
+        setLoading(false);
     }
 
     return (
+
         <div className={'list-card'}>
-            <div className={'list-title'}>
-                <p>{list.title}</p>
-                <div className={'edit-icon'}>
-                    <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
-                    <FontAwesomeIcon onClick={deleteList} icon={faTrash}></FontAwesomeIcon>
-                </div>
-            </div>
-            {itemCardItems.map((card) => (
+            {loading && <Loading/>}
+            {!loading &&
+                <div className={'list-title'}>
+                    <p>{list.title}</p>
+                    <div className={'edit-icon'}>
+                        <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+                        <FontAwesomeIcon onClick={deleteList} icon={faTrash}></FontAwesomeIcon>
+                    </div>
+                </div>}
+
+            {!loading && itemCardItems.map((card) => (
                 <div className={'task-container'} key={card.id}>{card.title}</div>
             ))}
+            {loading && itemCardItems && <Loading/>}
             <AddCardButton addCard={addCard} cardInput={cardInput} inputHandler={inputHandler}/>
         </div>
-    );
+    )
+        ;
 }
 
 export default List;
